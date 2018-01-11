6ee637b3d3c93d91eb056d7162b9dc1aad3b5088
upexcel <prateek@excellencetechnologies.in>
2018-01-11 18:28:04 +0530

    Register Page included

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators  } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators  } from '@angular/forms';
+import { patternValidator } from '../shared/pattern-validator';
+import { PasswordValidation } from '../shared/password-match';
+
 
 @Component({
   selector: 'app-register',
@@ -8,14 +11,19 @@ import { FormControl, FormGroup, Validators  } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
   
   registerForm: FormGroup;
-  
+
+  constructor( ) { }
+
   ngOnInit() {
     this.createForm();
   }
   private createForm() {
-    
+    this.registerForm = new FormGroup({
+      email:            new FormControl('',[Validators.required,patternValidator(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
+      password:         new FormControl('',[Validators.required]),
+      confirmPassword:  new FormControl('',[Validators.required]),
+    }, PasswordValidation.MatchPassword);
   }
 }
