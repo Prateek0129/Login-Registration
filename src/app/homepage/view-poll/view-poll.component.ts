8248600e91bc078853c0d4110ecf849709f8ff4e
Prateek0129 <prateek@excellencetechnologies.in>
2018-01-22 12:22:24 +0530

    Added Pagination

--- src/app/homepage/view-poll/view-poll.component.ts
+++ src/app/homepage/view-poll/view-poll.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../../http.service';
 
+import * as _ from 'lodash';
+
+import { PaginationService } from '../../pagination.service';
+
 @Component({
   selector: 'app-view-poll',
   templateUrl: './view-poll.component.html',
@@ -8,16 +12,29 @@ import { HttpService } from '../../http.service';
 })
 export class ViewPollComponent implements OnInit {
 
-  constructor(public httpService: HttpService) { }
-  polls: any;
+  constructor(public httpService: HttpService, public paginationService:PaginationService) { }
+  allItems: any;
+  pager:any = {};
+  pagedItems: any[];
   spin: boolean;
   ngOnInit() {
     this.spin = true;
     this.httpService.viewPolls().then((data) => {
       this.spin = false;
-      this.polls = data;
-      console.log(this.polls)
+      this.allItems = data;
+      this.setPage(1);
     });
   }
 
+  setPage(page: number) {
+    if (page < 1 || page > this.pager.totalPages) {
+        return;
+    }
+
+    // get pager object from service
+    this.pager = this.paginationService.getPager(this.allItems.length, page);
+
+    // get current page of items
+    this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
+}
 }
