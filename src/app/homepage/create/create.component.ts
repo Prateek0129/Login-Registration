c299e0305acaf9b51e72b6ed7187510e62dfc4c8
Prateek0129 <prateek@excellencetechnologies.in>
2018-02-01 12:48:03 +0530

    Required At least one option fixed.

--- src/app/homepage/create/create.component.ts
+++ src/app/homepage/create/create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpService } from '../../http.service';
 
 @Component({
@@ -10,34 +10,52 @@ import { HttpService } from '../../http.service';
 export class CreateComponent implements OnInit {
 
   createPollFormview: FormGroup;
-  spin:boolean;
-  message:string = null;
-  submit:boolean;
-  constructor(public httpService: HttpService) { }
+  spin: boolean;
+  message: string = null;
+  submit: boolean;
+  constructor(public httpService: HttpService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.createPollForm();
   }
+
   private createPollForm() {
-    this.createPollFormview = new FormGroup({
-      title: new FormControl('', Validators.required),
-      option: new FormControl('', Validators.required),
-    });
+    this.createPollFormview = this.fb.group({
+      title: '',
+      option1: undefined,
+      option2: undefined,
+      option3: undefined,
+      option4: undefined,
+    }, {
+      validator: (formGroup: FormGroup) => {
+        return this.validateForm(formGroup);
+      }
+      });
   }
 
+  validateForm(formgroup: FormGroup) {
+    if (formgroup.controls["title"].value && (formgroup.controls["option1"].value || formgroup.controls["option2"].value || formgroup.controls["option3"].value || formgroup.controls["option4"].value)) {
+      return null;
+    }
+    else {
+      return { validateForm: true };
+    }
+  }
+
+
   createPoll(data) {
-    this.spin = true;
-    this.submit = true;
-    this.httpService.createPoll(data).then((noerror) => {
-      if(noerror) {
-      this.message = "Your Pole Has Been SuccessFully Submitted";
-      this.spin = false;
-      this.createPollForm();
-      setTimeout(() => {
-        this.message = null;
-        this.submit = false;
-      }, 1000);
-      }
-  })
-}
+      this.spin = true;
+      this.submit = true;
+      this.httpService.createPoll(data).then((noerror) => {
+        if(noerror) {
+        this.message = "Your Pole Has Been SuccessFully Submitted";
+        this.spin = false;
+        this.createPollForm();
+        setTimeout(() => {
+          this.message = null;
+          this.submit = false;
+        }, 1000);
+        }
+    })
+  }
 }
